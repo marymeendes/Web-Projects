edd365102594fc6b77e1df0b88a2688068b275ea
Mariana Mendes da Silva <mariana.mendes98@hotmail.com>
2019-01-02 21:44:43 -0200

    Começando com JavaScript

---
+++ JSPractice/script.js
@@ -0,0 +1,35 @@
+function somar() {
+	var campo1 = parseInt(document.getElementById('campo1').value);
+	var campo2 = parseInt(document.getElementById('campo2').value);
+
+	var soma = campo1 + campo2;
+
+	alert(soma);
+}
+
+function subtrair() {
+	var campo1 = parseInt(document.getElementById('campo1').value);
+	var campo2 = parseInt(document.getElementById('campo2').value);
+
+	var soma = campo1 - campo2;
+
+	alert(soma);
+}
+
+function multiplicar() {
+	var campo1 = parseInt(document.getElementById('campo1').value);
+	var campo2 = parseInt(document.getElementById('campo2').value);
+
+	var soma = campo1 * campo2;
+
+	alert(soma);
+}
+
+function dividir() {
+	var campo1 = parseInt(document.getElementById('campo1').value);
+	var campo2 = parseInt(document.getElementById('campo2').value);
+
+	var soma = campo1 / campo2;
+
+	alert(soma);
+}
